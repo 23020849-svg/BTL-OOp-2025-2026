2bd5e0b3f7dc683eed3c39b8dcaeaa9d74c40b06
23020849-svg <23020849@vnu.edu.vn>
2025-09-30 14:47:36 +0700

    up date

--- src/GameView.java
+++ src/GameView.java
@@ -1,3 +1,74 @@
-public class GameView {
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class GameView extends JPanel implements ActionListener{
+    private boolean play= false;
+    private int totalBrick=21;
+    private final Timer timer;
+    private int delay=8;
+    private int ballposX=120;
+    private int ballposY=350;
+    private int ballX = -1;
+    private int ballY = -2;
+    private int playerX=350;
     
+    // kích thước & fps;
+    public static final int WIDTH = 750;
+    public static final int HEIGHT = 750;
+    private static final int FPS = 60;
+        // biến demo để thấy khung hình đổi theo thời gian
+    private long startNanoTime = System.nanoTime();;
+    private int frames = 0;
+    private double approxFPS = 0;
+
+    public GameView(){
+        //vẽ mượt hơn 
+        setDoubleBuffered(true);
+        setFocusable(true); //nhận phím
+
+        timer= new Timer(1000/ FPS, this);
+        //gọi hàm time
+        timer.start();
+
+        //mỗi tick: cập nhật logic game 
+     
+        
+        
+    }
+       private void updateGame(){
+            //sau này cập nhật sau
+            }
+
+    @Override
+    protected void paintComponent(Graphics g){
+        super.paintComponent(g);
+        //black canvas
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, getWidth(), getHeight());
+         //borders
+         g.setColor(Color.GREEN);
+            g.fillRect(0, 0, 3, 750);
+            g.fillRect(0, 0, 750, 3);
+            g.fillRect(getWidth()-3, 0, 3, 750);
+            g.fillRect(0,getHeight()-3, getWidth(), 3);
+        //the paddle
+        g.setColor(Color.PINK);
+        g.fillRect(playerX, 650, 100, 8);        
+
+        //the ball
+        g.setColor(Color.RED);
+        g.fillOval(ballposX, ballposY, 20, 20);
+       
+     
+    }
+     @Override
+    public void actionPerformed(ActionEvent e) {
+        updateGame();   // cập nhật trạng thái
+        repaint();      // yêu cầu vẽ lại
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,24 @@
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Image;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-
+import javax.swing.*;
 
 public class Main{
     public static void main(String[] args) {
-      
-      ImageIcon image1 = new ImageIcon("hahaa.png");
-      Image image = image1.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+        JFrame f= new JFrame();
+        f.setTitle("Arkanoid");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit out of application
+        f.setResizable(false);
+        
+        f.setSize(750,750);
+        ImageIcon image = new ImageIcon("rsc/prj.png"); //image icon
+        f.setIconImage(image.getImage());
 
-      JLabel label = new JLabel(); //create a label
-      label.setText("Welcome to my game"); //set text of label
-      label.setIcon(new ImageIcon(image));
-      label.setHorizontalTextPosition(JLabel.CENTER);//set text of iamge
-      label.setVerticalTextPosition(JLabel.TOP);
-      label.setForeground(Color.green);
-      label.setFont(new Font("MV Boli",Font.PLAIN,20));
-      MyFrame myFrame = new MyFrame();
-      myFrame.add(label);
-      
+        GameView gameview= new GameView();
+       
+        f.add(gameview);    
+       
+        
+        f.setVisible(true);
+
+          
     }
 
 }
