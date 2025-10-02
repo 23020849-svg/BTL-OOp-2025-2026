aea5c2581620acaf2bcde8f0252afe286eebe4c5
hieuphung219-sys <hieuphung219@gmail.com>
2025-10-02 22:00:26 +0700

    Update GameObject.java

--- src/GameObject.java
+++ src/GameObject.java
@@ -1,1 +1,29 @@
+// Up lên cho đỡ trống chưa biết đúng sai hay nào đâu hôm sau làm tiếp :))
+
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public abstract class GameObject {
+    protected double x, y;
+    protected int width, height;
+
+    public GameObject(double x, double y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public abstract void update(double delta);
+    public abstract void render(Graphics g);
+
+    public Rectangle getBounds() {
+        return new Rectangle((int)x, (int)y, width, height);
+    }
+
+    public double getX() { return x; }
+    public double getY() { return y; }
+    public int getWidth() { return width; }
+    public int getHeight() { return height; }
+}
 
