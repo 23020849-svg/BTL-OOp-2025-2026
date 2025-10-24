e5b7c269a08942ac36f134f6bde08fe5fa581a43
23020849-svg <23020849@vnu.edu.vn>
2025-10-24 19:12:05 +0700

    xóa arkanoid

--- arkanoid/FastBallPowerUp.java
+++
@@ -1,8 +0,0 @@
-public class FastBallPowerUp extends PowerUp {
-    //Tăng tốc độ bóng
-
-    public FastBallPowerUp(int x, int y, int width, int height, int duration) {
-        super(x, y, width, height, "FastBall", duration);
-    }
-    
-}
